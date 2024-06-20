e16a46ad3cbf5d0e1d8c5db8f9ecf9ff00920d74
Wolff404 <stavrosw@hotmail.com>
2024-06-20 23:14:13 +0200

    Made class for Circle.py
    Concidering to image with pygame.Surface instead of drawing them manually.

--- Classes/Circle.py
+++ Classes/Circle.py
@@ -1,12 +1,20 @@
 import pygame
-# Create class
 
+class Circle: # Maybe it's easier to use an image with the pygame.Surface than drawing the circles and X maunally
+    def __init__(self, x, y, col, row, mouse_x, mouse_y, cell_width, cell_height):
+        self.x = x
+        self.y = y
+        self.col = col
+        self.row = row
+        self.mouse_x = mouse_x
+        self.mouse_y = mouse_y
+        self.cell_width = cell_width
+        self.cell_height = cell_height
 
-
-def draw_circle():
-    mouse_x, mouse_y = pygame.mouse.get_pos()
-    col = mouse_x // TTT.cell_width
-    row = mouse_y // TTT.cell_height
-    TTT.grid[row][col] = pygame.draw.circle(TTT.screen, (255, 255, 255), (TTT.cell_width // 2, TTT.cell_height // 2), 50, 0)
+    def draw_circle(self):
+        mouse_x, mouse_y = pygame.mouse.get_pos()
+        col = mouse_x // self.cell_width
+        row = mouse_y // self.cell_height
+        self.grid[row][col] = pygame.draw.circle(self.screen, (255, 255, 255), (self.cell_width // 2, self.cell_height // 2), 50, 0)
 
 
