09f4f27855f59d20e1fd300d662004d227aeefb4
Wolff404 <stavrosw@hotmail.com>
2024-06-20 17:18:14 +0200

    There is now some description in the README.md file.
    Pregame screen file now has a class and a unfinished constructor.

--- Classes/Cross.py
+++ Classes/Cross.py
@@ -1,10 +1,23 @@
 import pygame
-import Tictactoe as TTT
+#Create class
+
+class Cross:
+
+    def __init__(self, x, y):  # Placeholder values
+        self.x = x
+        self.y = y
 
 
+'''
 def draw_cross():
     mouse_x, mouse_y = pygame.mouse.get_pos()
     col = mouse_x // TTT.cell_width
     row = mouse_y // TTT.cell_height
     TTT.grid[row][col] = pygame.draw.line(TTT.screen, TTT.RANDOM_COLOR,(100,100),(200,200))
     print("HEY")
+'''
+
+
+
+
+
---
+++ Classes/Pregame Screen.py
@@ -0,0 +1,16 @@
+# Create class
+import pygame
+
+
+class Pregame_Screen:
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+    # def draw_UI(self, screen): To draw the screen
+
+    # def click(self, event): To manage player settings.
+
+    # Like which player gets to go first and chosing the cirlce and X.
