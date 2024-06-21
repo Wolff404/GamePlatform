6d0fb19cf517a4c68d3ffb36a3a0129c7bb1ade4
Wolff404 <stavrosw@hotmail.com>
2024-06-21 16:54:44 +0200

    Some foundation work is now in the Pregame Screen.py file. However, I think I am going in the wrong direction with this one.

--- Classes/Pregame Screen.py
+++ Classes/Pregame Screen.py
@@ -2,14 +2,24 @@
 import pygame
 
 
-class Pregame_Screen:
-    def __init__(self, x, y, width, height):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
+class Pregame_Screen: # Problems may occur because mindset was making a pregame screen, but actually made checkboxes.
+    def __init__(self, x, y, width, height, screen):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.color = (0, 0, 0)
+        self.checked = False
+        self.screen = (400, 400)
 
-    # def draw_UI(self, screen): To draw the screen
+    def draw(self, screen):
+        screen = pygame.display.set_mode((self.screen))
+        pygame.draw.rect(screen, self.color, self.rect)
+        pygame.display.set_caption("Welcome to Tic Tac Toe")
+
+        font = pygame.font.Font(None, 40)
+        if self.checked:
+            pygame.draw.line(screen, self.color,(self.rect.left, self.rect.top), (self.rect.right, self.rect.bottom), 2)
+            pygame.draw.line(screen, self.color,(self.rect.left, self.rect.bottom), (self.rect.right, self.rect.top), 2)
+
+
 
     # def click(self, event): To manage player settings.
 
