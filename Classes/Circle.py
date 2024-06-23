dfbbe7c5d944ef9575295110df64923dac55ac32
Wolff404 <stavrosw@hotmail.com>
2024-06-23 19:03:22 +0200

    Attempting to cut down on unnecessary code without ruining something that already does not work!

--- Classes/Circle.py
+++ Classes/Circle.py
@@ -2,14 +2,11 @@ import pygame
 
 from Classes import Grid
 
-#
+
 class Circle:
-    def __init__(self, grid: Grid, col, row, mouse_x, mouse_y, cell_width, cell_height,screen):
-        self.grid = grid  # Uh? Is this ok?
+    def __init__(self, col, row, cell_width, cell_height, screen):
         self.grid.col = col
         self.grid.row = row
-        self.mouse_x = mouse_x
-        self.mouse_y = mouse_y
         self.cell_width = cell_width
         self.cell_height = cell_height
         self.screen = screen
@@ -18,6 +15,7 @@ class Circle:
         mouse_x, mouse_y = pygame.mouse.get_pos()
         col = mouse_x // self.cell_width
         row = mouse_y // self.cell_height
-        self.grid[row][col] = pygame.draw.circle(self.screen, (255, 255, 255), (self.cell_width // 2, self.cell_height // 2), 50, 0)
+        # Grid.grid[row][col] = pygame.draw.circle(self.screen, (255, 255, 255),
+        #                                        (self.cell_width // 2, self.cell_height // 2), 50, 0)
 
 
