1f1a5bddc5b02308d0a3b091a0212718c37acbe0
Wolff404 <stavrosw@hotmail.com>
2024-06-24 21:51:09 +0200

    Removed some unecessary code. However the entire thing is going to change so the change is redundant.

--- Tictactoe.py
+++ Tictactoe.py
@@ -1,13 +1,12 @@
 import pygame
 from Classes.Grid import Grid
-from Classes.Circle import Circle
+
 
 pygame.init()
 
 pygame.display.set_caption('Tic Tac Toe')
 
 grid_object = Grid()
-#circle_object = Circle(grid_object.grid, grid_object.col, grid_object.row, )
 
 running = True
 while running:
@@ -17,7 +16,6 @@ while running:
 
 
     grid_object.draw_grid()
-    #circle_object.draw_circle()
     pygame.display.flip()
 
 
