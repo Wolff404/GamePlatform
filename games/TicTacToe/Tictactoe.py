cafd6dfd4db4ba97ee2cc06482a311727959e9dc
Wolff404 <stavrosw@hotmail.com>
2024-06-26 18:29:49 +0200

    Removed current logic from Tictactoe.py

--- games/TicTacToe/Tictactoe.py
+++ games/TicTacToe/Tictactoe.py
@@ -1,22 +1,3 @@
 import pygame
-from æignore.øOldClasses.Grid import Grid
 
 
-pygame.init()
-
-pygame.display.set_caption('Tic Tac Toe')
-
-grid_object = Grid()
-
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-
-    grid_object.draw_grid()
-    pygame.display.flip()
-
-
-pygame.quit()
