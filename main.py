24cc17a0a18a6fdb1202e1065360471523d2f83e
Wolff404 <stavrosw@hotmail.com>
2024-06-26 18:42:58 +0200

    Added more empty functions where functionality will be added later with some comments for direction.

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+from menu import Menu
+
+
+def main():
+    pass  # pygame.init(), screen display, caption, menu function call, menu display call, pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
---
+++ menu.py
@@ -0,0 +1,12 @@
+import pygame
+
+
+class Menu:
+    def __init__(self):
+        pass
+
+    def display_menu(self):  # Add nested while loop with event
+        pass
+
+    def launch_game(self):
+        pass
