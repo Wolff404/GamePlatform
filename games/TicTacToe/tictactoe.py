66a48badff31dcf21c16fda395ba618827fd0e17
Wolff404 <stavrosw@hotmail.com>
2024-07-15 17:40:10 +0200

    Small syntax adjustments

--- games/TicTacToe/tictactoe.py
+++ games/TicTacToe/tictactoe.py
@@ -11,12 +11,12 @@ def run():
     pygame.display.set_caption("Tictactoe")
 
     def draw_grid():
-        background_color = (230,230,230)
+        background_color = (230, 230, 230)
         grid = (100, 100, 100)
         screen.fill(background_color)
-        for x in range(1,3):
-            pygame.draw.line(screen,grid,(0,x*200),(screen_width,x*200))
-            pygame.draw.line(screen,grid,(x*200,0),(x*200,screen_height))
+        for x in range(1, 3):
+            pygame.draw.line(screen,grid,(0, x*200),(screen_width, x*200))
+            pygame.draw.line(screen,grid,(x*200, 0),(x*200, screen_height))
 
 
 
