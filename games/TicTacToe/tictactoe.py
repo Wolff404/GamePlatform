2ecc5a68025d5c803fa1393456c089f74d3c2641
Wolff404 <stavrosw@hotmail.com>
2024-07-16 22:42:26 +0200

    Alignment when drawing X and O now works correctly, by changing incorrect hardcoded offsets that did not align with the cell size.

--- games/TicTacToe/tictactoe.py
+++ games/TicTacToe/tictactoe.py
@@ -1,5 +1,6 @@
 import pygame
 
+
 def run():
     pygame.init()
 
@@ -8,7 +9,6 @@ def run():
     screen = pygame.display.set_mode((screen_width, screen_height))
 
     pygame.display.set_caption("Tictactoe")
-
 
     clicked = False
     spaces = []
@@ -24,25 +24,32 @@ def run():
         grid = (100, 100, 100)
         screen.fill(background_color)
         for x in range(1, 3):
-            pygame.draw.line(screen, grid, (0, x*200), (screen_width, x*200), 3)  # Drawing the horizontal lines
-            pygame.draw.line(screen, grid, (x*200, 0), (x*200, screen_height), 3)  # Drawing the vertical lines
-
-
+            pygame.draw.line(screen, grid, (0, x * 200), (screen_width, x * 200), 3)  # Drawing the horizontal lines
+            pygame.draw.line(screen, grid, (x * 200, 0), (x * 200, screen_height), 3)  # Drawing the vertical lines
 
     def place_space():
-        black = (0,0,0)
+        black = (0, 0, 0)
         line_thickness = 3
+        cell_size = 200  # Size of each cell
 
         x_position = 0
-        for x in spaces:  # TODO: Fix this...
+        for x in spaces:  # Checks for x in the grid
             y_position = 0
-            for y in x:
-                if y == 1:
-                    pygame.draw.line(screen, black, (x_position * 250 + 15, y_position * 250 + 15),(x_position * 250 + 85, y_position * 250 + 85), line_thickness)
-                    pygame.draw.line(screen, black, (x_position * 250 + 85, y_position * 250 + 15), (x_position * 250 + 15, y_position * 250 + 85), line_thickness)
+            for y in x:  # Checks for y in the grid
+                center_x = x_position * cell_size + cell_size // 2  # Calculating x coordinate of the centre of the
+                # current cell
+                center_y = y_position * cell_size + cell_size // 2  # Calculating y coordinate of the centre of the
+                # current cell
+                if y == 1:  # Drawing lines to make an X
+                    offset = cell_size // 3  # Ensuring that the drawing's size fits the grid cell well enough
+                    pygame.draw.line(screen, black, (center_x - offset, center_y - offset),
+                                     (center_x + offset, center_y + offset), line_thickness)
+                    pygame.draw.line(screen, black, (center_x + offset, center_y - offset),
+                                     (center_x - offset, center_y + offset), line_thickness)
 
-                if y == -1:
-                    pygame.draw.circle(screen, black, (x_position * 200 + 50, y_position * 200 + 50), 38, line_thickness)
+                if y == -1:  # Drawing O
+                    radius = cell_size // 3  # Ensuring that the drawing's size fits the grid cell well enough
+                    pygame.draw.circle(screen, black, (center_x, center_y), radius, line_thickness)
 
                 y_position += 1
             x_position += 1
@@ -59,8 +66,8 @@ def run():
             if event.type == pygame.MOUSEBUTTONDOWN and clicked == False:
                 clicked = True
             if event.type == pygame.MOUSEBUTTONUP and clicked == True:  # For checking a mouse click cycle
-                clicked = False  # Now implementing logic for when the mousebutton is released.
-                mouse_position = pygame.mouse.get_pos()  # Where the mousebutton was released
+                clicked = False  # Now implementing logic for when the mouse-button is released.
+                mouse_position = pygame.mouse.get_pos()  # Where the mouse-button was released
                 cell_x = mouse_position[0] // 200  # Defining x coordinate
                 cell_y = mouse_position[1] // 200  # Defining y coordinate
                 if spaces[cell_x][cell_y] == 0:  # Checking if anything has been clicked.
@@ -70,5 +77,3 @@ def run():
         pygame.display.update()
 
     pygame.quit()
-
-
