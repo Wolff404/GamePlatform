5ef775ed626e80aa8ef8366193fed8b5d640ecb0
Wolff404 <stavrosw@hotmail.com>
2024-07-16 18:58:19 +0200

    Attempting to fix function within constructor

--- games/TicTacToe/tictactoe.py
+++ games/TicTacToe/tictactoe.py
@@ -1,20 +1,14 @@
 import pygame
 
-
-
 def run():
-
     pygame.init()
 
     screen_width = 600
     screen_height = 600
     screen = pygame.display.set_mode((screen_width, screen_height))
 
-    pygame.display.set_caption("Tictactoe")  # Title of the game.
-
-    cross_drawing = pygame.image.load('assets/images/tictactoeCROSS.png')
-
-    # Variables
+    pygame.display.set_caption("Tictactoe")
+    cross_drawing = 'assets/images/tictactoeCROSS.png'
 
     clicked = False
     spaces = []
@@ -28,21 +22,18 @@ def run():
             self.rect.topleft = (x, y)
             self.clicked = False
 
-        def draw(self):
-
+        def draw(self,x,y,image):
             action = False
-
             mouse_pos = pygame.mouse.get_pos()
-
             if self.rect.collidepoint(mouse_pos):
                 if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
                     self.clicked = True
                     action = True
-
             screen.blit(self.image, (self.rect.x, self.rect.y))
-
             return action
 
+
+    TicTacToe.draw(200,200,cross_drawing)
     def draw_grid():
         background_color = (230, 230, 230)
         grid = (100, 100, 100)
@@ -51,35 +42,24 @@ def run():
             pygame.draw.line(screen, grid, (0, x*200), (screen_width, x*200), 3)  # Drawing the horizontal lines
             pygame.draw.line(screen, grid, (x*200, 0), (x*200, screen_height), 3)  # Drawing the vertical lines
 
-    for _ in range(3):  # Creating the 3x3 matrice
-        row = [0] * 3
-        spaces.append(row)
-
+        for _ in range(3):  # Creating the 3x3 matrice
+            row = [0] * 3
+            spaces.append(row)
 
     def place_space():
         x_position = 0
         for x in spaces:
             y_position = 0
-            #for y in x:
-                #if y == 1:
-            #TicTacToe.draw()
 
-
-
-
+    draw_grid()
 
     running = True
     while running:
-
-        draw_grid()
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-
             if event.type == pygame.MOUSEBUTTONDOWN and clicked == False:
                 clicked = True
-
             if event.type == pygame.MOUSEBUTTONUP and clicked == True:  # For checking a mouse click cycle
                 clicked = False  # Now implementing logic for when the mousebutton is released.
                 mouse_position = pygame.mouse.get_pos()  # Where the mousebutton was released
@@ -88,7 +68,8 @@ def run():
                 if spaces[cell_x // 200][cell_y // 200] == 0:  # Checking if anything has been clicked.
                     spaces[cell_x // 200][cell_y // 200] = player  # Player 1 goes first
                     player *= -1  # Changing to player 2 ( Player -1) by multiplying 1 with -1 and vice versa
-
         pygame.display.update()
 
     pygame.quit()
+
+
